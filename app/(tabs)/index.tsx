46d3a463560d4aa5bf6787b628d4276ca3e1a926
SeuNomeDeUsuario <seuemail@exemplo.com>
2026-04-13 23:39:15 -0300

    Adicionando inputs de origem e destino

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,36 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Home() {
+  const [origem, setOrigem] = useState("");
+  const [destino, setDestino] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>PWA Rotas 🚀</Text>
-      <Text style={styles.subtitle}>Seu app de rotas está começando!</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a origem"
+        value={origem}
+        onChangeText={setOrigem}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o destino"
+        value={destino}
+        onChangeText={setDestino}
+      />
 
       <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Buscar Rota</Text>
+        <Text style={styles.buttonText}>Calcular Rota</Text>
       </TouchableOpacity>
     </View>
   );
@@ -17,25 +40,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
     padding: 20,
   },
   title: {
     fontSize: 26,
     fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
   },
-  subtitle: {
-    marginVertical: 10,
-    fontSize: 16,
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 12,
+    marginBottom: 10,
+    borderRadius: 8,
   },
   button: {
-    marginTop: 20,
     backgroundColor: "#000",
     padding: 15,
     borderRadius: 10,
+    marginTop: 10,
   },
   buttonText: {
     color: "#fff",
+    textAlign: "center",
     fontSize: 16,
   },
 });
