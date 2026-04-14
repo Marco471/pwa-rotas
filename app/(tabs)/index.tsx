84d9e27fe53a18b27c2f367c016cbdf076eb2c42
SeuNomeDeUsuario <seuemail@exemplo.com>
2026-04-13 23:09:17 -0300

    Adicionando botão buscar rota na tela inicial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>PWA Rotas 🚀</Text>
-      <Text>Seu app de rotas está começando!</Text>
+      <Text style={styles.subtitle}>Seu app de rotas está começando!</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Buscar Rota</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,9 +18,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 26,
     fontWeight: "bold",
+  },
+  subtitle: {
+    marginVertical: 10,
+    fontSize: 16,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#000",
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
   },
 });
